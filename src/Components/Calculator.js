d0e9c6e51ec09683d221d129a444d06f854a0e29
Esmatullah Ahmadzai <ea.ahmadzai2020@gmail.com>
2022-08-10 17:51:00 +0430

    Remove un-use code

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -9,10 +9,9 @@ class Calculator extends Component {
       operation: null,
       next: null,
     };
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick = (e) => {
+  btnClick = (e) => {
     this.setState((obj) => calculate(obj, e.target.innerText));
   }
 
@@ -24,69 +23,69 @@ class Calculator extends Component {
           <div className="col border p-4 bg-secondary text-light ">{(total || '') + (operation || '') + (next || '') || 0 }</div>
         </div>
         <div className="row">
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             AC
           </button>
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             +/-
           </button>
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             %
           </button>
-          <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={this.btnClick}>
             &divide;
           </button>
         </div>
         <div className="row">
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             7
           </button>
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             8
           </button>
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             9
           </button>
-          <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={this.btnClick}>
             x
           </button>
         </div>
         <div className="row">
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             4
           </button>
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             5
           </button>
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             6
           </button>
-          <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={this.btnClick}>
             -
           </button>
         </div>
         <div className="row">
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             1
           </button>
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             2
           </button>
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             3
           </button>
-          <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={this.btnClick}>
             +
           </button>
         </div>
         <div className="row">
-          <button type="button" className="col-6 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-6 text-center border p-3" onClick={this.btnClick}>
             0
           </button>
-          <button type="button" className="col-3 text-center border p-3" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3" onClick={this.btnClick}>
             .
           </button>
-          <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={this.handleClick}>
+          <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={this.btnClick}>
             =
           </button>
         </div>
