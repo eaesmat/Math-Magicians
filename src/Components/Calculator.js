28d51a70ca58ca373dfbd32cd91a3d6778ea66c7
Esmatullah Ahmadzai <ea.ahmadzai2020@gmail.com>
2022-08-16 16:28:03 +0430

    Add full website

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -9,78 +9,170 @@ const Calculator = () => {
   };
   const { total, operation, next } = calAction;
   return (
-    <div>
-      <div className="row">
-        <div className="col border p-4 bg-secondary text-light ">{(total || '') + (operation || '') + (next || '') || 0 }</div>
+    <>
+      <div className="container justify-content-between align-items-between ps-5">
+        <div className="row">
+          <div className="col-4 pt-5 mt-5">
+            <h1>Lets do some Maths!</h1>
+          </div>
+
+          <div className="col-5">
+            <div className="col mt-5 pt-5">
+              <div>
+                <div className="row">
+                  <div className="col border p-4 bg-secondary text-light ">
+                    {(total || '') + (operation || '') + (next || '') || 0}
+                  </div>
+                </div>
+                <div className="row">
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    AC
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    +/-
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    %
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3 bg-warning"
+                    onClick={btnClick}
+                  >
+                    &divide;
+                  </button>
+                </div>
+                <div className="row">
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    7
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    8
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    9
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3 bg-warning"
+                    onClick={btnClick}
+                  >
+                    x
+                  </button>
+                </div>
+                <div className="row">
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    4
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    5
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    6
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3 bg-warning"
+                    onClick={btnClick}
+                  >
+                    -
+                  </button>
+                </div>
+                <div className="row">
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    1
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    2
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    3
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3 bg-warning"
+                    onClick={btnClick}
+                  >
+                    +
+                  </button>
+                </div>
+                <div className="row">
+                  <button
+                    type="button"
+                    className="col-6 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    0
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3"
+                    onClick={btnClick}
+                  >
+                    .
+                  </button>
+                  <button
+                    type="button"
+                    className="col-3 text-center border p-3 bg-warning"
+                    onClick={btnClick}
+                  >
+                    =
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="row">
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          AC
-        </button>
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          +/-
-        </button>
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          %
-        </button>
-        <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={btnClick}>
-          &divide;
-        </button>
-      </div>
-      <div className="row">
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          7
-        </button>
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          8
-        </button>
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          9
-        </button>
-        <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={btnClick}>
-          x
-        </button>
-      </div>
-      <div className="row">
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          4
-        </button>
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          5
-        </button>
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          6
-        </button>
-        <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={btnClick}>
-          -
-        </button>
-      </div>
-      <div className="row">
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          1
-        </button>
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          2
-        </button>
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          3
-        </button>
-        <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={btnClick}>
-          +
-        </button>
-      </div>
-      <div className="row">
-        <button type="button" className="col-6 text-center border p-3" onClick={btnClick}>
-          0
-        </button>
-        <button type="button" className="col-3 text-center border p-3" onClick={btnClick}>
-          .
-        </button>
-        <button type="button" className="col-3 text-center border p-3 bg-warning" onClick={btnClick}>
-          =
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/Pages/QuotePage.js
@@ -0,0 +1,17 @@
+const QuotePage = () => (
+  <div className="container justify-content-center align-items-center pt-5 mt-5">
+    <div className="row ">
+      <div className="col">
+        <h3 className="text-center pt-5 mt-5">
+          <q>
+            Mathematics has beauty and romance. It’s not a boring place to be,
+            the mathematical world. It’s an extraordinary place; it’s worth
+            spending time there.
+          </q>
+        </h3>
+      </div>
+    </div>
+  </div>
+);
+
+export default QuotePage;
